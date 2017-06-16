ef0097af2133dd0dd0ae95cab73f63c4fdba94a9
AlexanderPokidin <a.pokidin@gmail.com>
2017-06-16 17:00:56 +0300

    Messenger is done

--- app/src/main/java/messenger/hfad/com/messenger/CreateMessageActivity.java
+++ app/src/main/java/messenger/hfad/com/messenger/CreateMessageActivity.java
@@ -1,11 +1,12 @@
 package messenger.hfad.com.messenger;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-public class CreateMessageActivity extends AppCompatActivity {
+public class CreateMessageActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +14,10 @@ public class CreateMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_message);
     }
     public void onSendMessage(View view){
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
         startActivity(intent);
     }
 }
